kohei-matsuyama/server

commit 91fe87c7a872ba6c14850bad72b8ec99d383b38d

MDEV-22166: Fix cmake -DPLUGIN_PARTITION=NO
mysql_write_frm(): Correctly enclose code inside
#ifdef WITH_PARTITION_STORAGE_ENGINE
so that cmake -DPLUGIN_PARTITION=NO builds can succeed.
This was broken in
commit b7bba721eee42af892178a747fa72a6fc2975c94.

sql/sql_table.cc
@@ -722,10 +722,10 @@ bool mysql_write_frm(ALTER_PARTITION_PARAM_TYPE *lpt, uint flags)
   char path[FN_REFLEN+1];
   char shadow_path[FN_REFLEN+1];
   char shadow_frm_name[FN_REFLEN+1];
-  char bak_path[FN_REFLEN+1];
-  char bak_frm_name[FN_REFLEN+1];
   char frm_name[FN_REFLEN+1];
 #ifdef WITH_PARTITION_STORAGE_ENGINE
+  char bak_path[FN_REFLEN+1];
+  char bak_frm_name[FN_REFLEN+1];
   char *part_syntax_buf;
   uint syntax_len;
   partition_info *part_info= lpt->part_info;
@@ -872,8 +872,8 @@ bool mysql_write_frm(ALTER_PARTITION_PARAM_TYPE *lpt, uint flags)
                                                           CHF_RENAME_FLAG))
       DBUG_RETURN(TRUE);
   }
-#else /* !WITH_PARTITION_STORAGE_ENGINE */
   DBUG_ASSERT(!(flags & WFRM_WRITE_EXTRACTED));
+#else /* !WITH_PARTITION_STORAGE_ENGINE */
   DBUG_ASSERT(!(flags & WFRM_BACKUP_ORIGINAL));
 #endif /* !WITH_PARTITION_STORAGE_ENGINE */
   if (flags & WFRM_INSTALL_SHADOW)

commit 155e1c2836b8473466a1bdba2f37e8992c035b72

MDEV-22166: Fix cmake -DPLUGIN_PARTITION=YES
This was broken in
commit 91fe87c7a872ba6c14850bad72b8ec99d383b38d

sql/sql_table.cc
@@ -708,8 +708,8 @@ uint build_table_shadow_filename(char *buff, size_t bufflen,
 
 
 /*
-  TODO: Partitioning atomic DDL refactoring: WFRM_WRITE_SHADOW and
-  WFRM_WRITE_EXTRACTED should be merged with create_table_impl(frm_only == true).
+  TODO: Partitioning atomic DDL refactoring: WFRM_WRITE_SHADOW
+  should be merged with create_table_impl(frm_only == true).
 */
 bool mysql_write_frm(ALTER_PARTITION_PARAM_TYPE *lpt, uint flags)
 {
@@ -872,7 +872,6 @@ bool mysql_write_frm(ALTER_PARTITION_PARAM_TYPE *lpt, uint flags)
                                                           CHF_RENAME_FLAG))
       DBUG_RETURN(TRUE);
   }
-  DBUG_ASSERT(!(flags & WFRM_WRITE_EXTRACTED));
 #else /* !WITH_PARTITION_STORAGE_ENGINE */
   DBUG_ASSERT(!(flags & WFRM_BACKUP_ORIGINAL));
 #endif /* !WITH_PARTITION_STORAGE_ENGINE */